3a2d565b1c5552313b10e3a54b2518384d3eb5c3
Goodness Duru <goodyduru@gmail.com>
2023-11-24 13:47:57 +0100

    Add parser test

--- src/tests.c
+++ src/tests.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "header.h"
 #include "nitrows.h"
 
 void echo_message(int key, uint8_t *message, uint64_t length) {
@@ -7,6 +8,19 @@ void echo_message(int key, uint8_t *message, uint64_t length) {
   if (!is_sent) {
     nitrows_close(key);
   }
+}
+
+int test_extension_parser() {
+  char tokens[] = "foo,bar;baz,foo;baz";
+  char tokens1[] = "foo;bar;baz=1;bar=2";
+  char tokens2[] = "bar;quote=\"10\",gaga";
+  ExtensionList *list = get_extension_list(1);
+  parse_extensions(1, tokens, list);
+  parse_extensions(1, tokens1, list);
+  parse_extensions(1, tokens2, list);
+  printf("Testing tokens: %s\n", tokens);
+  print_list(list);
+  return 0;
 }
 
 int main() {
